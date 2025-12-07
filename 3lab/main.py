3a17cb89723be66e31c9741a81645978e735eefa
valeriarrr <valerosheva@gmail.com>
2025-12-07 23:34:38 +0300

    main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,48 @@
+def recursion(new_line, results=[]):
+    new_line = new_line.strip()
 
+    if new_line.isdigit() or (new_line.startswith('-') and new_line[1:].isdigit()):
+        results.append((new_line, int(new_line)))
+        return int(new_line), results
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+    for i in range(len(new_line) - 1, -1, -1):
+        char = new_line[i]
+        if char == '+':
+            left_part = new_line[:i]
+            right_part = new_line[i+1:]
+            left_result, results = recursion(left_part, results)
+            right_result, results = recursion(right_part, results)
+            result = left_result + right_result
+            results.append((f'{left_part} + {right_part} = {result}'))
+            return result, results
+        elif char == '-':
+            if i > 0:
+                left_part = new_line[:i]
+                right_part = new_line[i+1:]
+                left_result, results = recursion(left_part, results)
+                right_result, results = recursion(right_part, results)
+                result = left_result - right_result
+                results.append((f'{left_part} - {right_part} = {result}'))
+                return result, results
+
+    for i in range(len(new_line) - 1, -1, -1):
+        char = new_line[i]
+        if char == '*':
+            left_part = new_line[:i]
+            right_part = new_line[i+1:]
+            left_result, results = recursion(left_part, results)
+            right_result, results = recursion(right_part, results)
+            result = left_result * right_result
+            results.append((f'{left_part} * {right_part} = {result}'))
+            return result, results
+        elif char == '/':
+            left_part = new_line[:i]
+            right_part = new_line[i+1:]
+            left_result, results = recursion(left_part, results)
+            right_result, results = recursion(right_part, results)
+            result = left_result / right_result
+            results.append((f'{left_part} / {right_part} = {result}'))
+            return result, results
+
+result, results_list = recursion('10/5+2-1+14/2*1')
+print(results_list)
